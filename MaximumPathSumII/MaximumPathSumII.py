d03a5daf629ab3916c1a21f30eb1a5d15e9f6d7e
SeanCagin <cuz3@gatech.edu>
2023-10-01 19:00:53 -0700

    moved comments to README

--- MaximumPathSumII/MaximumPathSumII.py
+++ MaximumPathSumII/MaximumPathSumII.py
@@ -1,17 +1,3 @@
-# We will put the whole structure in an array or arrays.
-# This is a tree-like structure but two nodes can share children
-# The left child of triangle[i][j] = triangle[i+1][j]
-# Similarly, the right child is triangle[i+1][j+1]
-# We start adding from the bottom thus only going each element once, making our algorithm O(n)
-# Example:
-#              3         ->           3       ->      3     ->    23
-#            /  \                   /  \            /  \
-#           7    4                 7    4          20  19
-#         /   \/  \              /   \/  \
-#        2    4    6            10   13   15
-#       /  \/   \/  \
-#      8   5    9   3
-
 t = int(input())  # The number of test cases
 
 for i in range(0, t):
